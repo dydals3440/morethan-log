61f69236c983b9ce73c156cd871a956e917417ca
김용민 <dydals3440@gmail.com>
2023-07-05 21:53:48 +0900

    docs :site.config수정

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "dydals3440",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    bio: "FE Change The World.",
+    email: "dydals3440@gmail.com",
+    linkedin: "dydals3440",
+    github: "dydals3440",
+    instagram: "yong__min_kim",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `YOLOG`,
+      href: "https://github.com/dydals3440/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "YOLOG",
+    description: "welcome to YOLOG!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
